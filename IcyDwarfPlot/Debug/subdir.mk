713ccf467d6a4ee074d9b3be714735fc82689248
MarcNeveu <nifiou@gmail.com>
2014-03-05 11:38:14 -0700

    IcyDwarfPlot v14.3.1
    The plotting functions of IcyDwarf are now removed from the main
    program, IcyDwarf, and implemented in this program, IcyDwarfPlot.
    This allows IcyDwarf to be run without needing Simple DirectMedia Layer
    2 (SDL2), needed by the plotting functions. It also allows IcyDwarfPlot
    to be run on any IcyDwarf output files without having to re-run IcyDwarf
    each time.

---
+++ IcyDwarfPlot/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../IcyDwarfPlot.c 
+
+OBJS += \
+./IcyDwarfPlot.o 
+
+C_DEPS += \
+./IcyDwarfPlot.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -I/usr/include -I/Library/Frameworks/SDL2.framework/Versions/A/Headers -I/Library/Frameworks/SDL2_image.framework/Versions/A/Headers -I/Library/Frameworks/SDL2_mixer.framework/Versions/A/Headers -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/Cocoa.framework/Versions/A/Headers -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/GLUT.framework/Versions/A/Headers -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/OpenGL.framework/Versions/A/Headers -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
